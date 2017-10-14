aaaaba84fbffef6f30823178ba5e85baec4f0052
Lowell Stang <lrstang@gmail.com>
2017-10-14 15:00:36 -0500

    Finals for plots 1-3

--- plot1.R
+++ plot1.R
@@ -1,6 +1,25 @@
+#
+# Code to reproduce Week 1, Assignment 1, Chart 1 for Exploratory Data Analysis Course
+#
+# Assignment information can be found in forked github repo here:  https://github.com/lrstang/ExData_Plotting1
+#
+#
+
+# read household power consumption data - '?' == NA
 hpc<-read.table("household_power_consumption.txt",sep=";", header=TRUE,na.strings="?")
+
+# convert distinct date/time into datetime variable for ease of use
 hpc$DateTime<-strptime(paste(hpc$Date,hpc$Time),"%d/%m/%Y %H:%M:%S")
+
+# subset data on dates specified in assignment instructions
 hpcFiltered<-subset(hpc, DateTime > "2007-02-01" & DateTime < "2007-02-03")
+
+# remove large, unneeded input dataframe
+rm(hpc)
+
+# create histogram with red frequency bars and appropriate titles and axis labels
 hist(hpcFiltered$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)",ylim=c(0,1200))
+
+# create 480x480 pixel PNG output and close device
 dev.copy(png,file="plot1.png")
 dev.off()
--- plot3.R
+++ plot3.R
@@ -1,8 +1,9 @@
 #
-# Code to reproduce Week 1, Assignment 1, Chart 2 for Exploratory Data Analysis Course
+# Code to reproduce Week 1, Assignment 1, Chart 3 for Exploratory Data Analysis Course
 #
 # Assignment information can be found in forked github repo here:  https://github.com/lrstang/ExData_Plotting1
 #
+# Note the PNG output legend is not properly rendered in RStudio (a feature).  Use regular R.
 #
 
 # read household power consumption data - '?' == NA
@@ -17,9 +18,15 @@ hpcFiltered<-subset(hpc, DateTime > "2007-02-01" & DateTime < "2007-02-03")
 # remove large, unneeded input dataframe
 rm(hpc)
 
-# create line chart of Global Active Power variable w/appropriate axis labels
-with (hpcFiltered, plot(DateTime,Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab=""))
+# create line chart of 3 sub-metering variables  w/legend
+with (hpcFiltered, {
+      plot(DateTime,Sub_metering_1,type="l",ylab="Energy sub metering",xlab="")
+      points(DateTime, Sub_metering_2, type="l", col="red")
+      points(DateTime, Sub_metering_3, type="l", col="blue")
+      legend("topright", col=c("black","blue","red"), legend=c("Sub_metering_1 ", "Sub_metering_2 ", "Sub_metering_3 "),lty=1,cex=0.9)
+      }
+)
 
 # create 480x480 pixel PNG output and close device
-dev.copy(png,file="plot2.png")
+dev.copy(png,file="plot3.png")
 dev.off()
